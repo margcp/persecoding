b38331881ef54d1021f149acd780a4bc44d54df6
margcp <125534417+margcp@users.noreply.github.com>
2023-03-07 22:43:10 +0800

    Create task6.py

---
+++ specimenp1/task6.py
@@ -0,0 +1,15 @@
+a= input("first name: ")
+b= input("second name: ")
+c= input("third name: ")
+d= input("fourth name: ")
+e= input("fifth name: ")
+if a=="Ellie":
+  print("1st")
+if b=="Ellie":
+  print("2nd")
+if c=="Ellie":
+  print("3rd")
+if d=="Ellie":
+  print("4th")
+if e=="Ellie":
+  print("5th")
